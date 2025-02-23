10e68414010a0753812994ea824ea429744fbbbe
Lebeloane <lebeloane@tutamail.com>
2025-02-23 16:59:59 +0200

    Display menu by category function

--- index.js
+++ index.js
@@ -1,47 +1,50 @@
 // Sample menu data (Consider fetching this data from a server in a real-world scenario)
 const menu = {
     Starters: [
-        {name: "Garlic Bread", price: 15} , 
-        {name: "Bruschetta", price: 40}
+        {name: "Garlic Bread", price: 18} , 
+        {name: "Bruschetta", price: 43}
     ],
     MainCourses: [
-        {name: "Margherita Pizza", price: 140},
-        {name: "Spaghetti Carbonara", price: 66}
+        {name: "Margherita Pizza", price: 146},
+        {name: "Spaghetti Carbonara", price: 72}
         ],
     Desserts: [
-        {name:"Tiramisu", price: 300},
-        {name: "Cheesecake", price: 250}
+        {name:"Tiramisu", price: 345},
+        {name: "Cheesecake", price: 265}
     ]
 };
 
 // Function to display menu items by category
 function displayMenuItems(menu) {
     // Get the menu container element from the HTML
-
+    const displayMenu = document.getElementById('menu')
     // Loop through each category and its items in the menu object
-
+    for (const [category, items] of Object.entries(menu)){
         // Create an element to represent the category
-
+        const categoryElement = document.createElement('h2')
         // Set the text content of the category element to the category name
-
+        categoryElement.textContent = category
         // Append the category element to the menu container
-
+        displayMenu.appendChild(categoryElement)
         // Create an element to represent a list of items
-
+        const listItems = document.createElement('ul')
         // Append a list of items element to the menu container
-
+        displayMenu.appendChild(listItems)
         // Loop through the items in the category and create list items
-
+        items.forEach(item => {
             // Create a list item element
-
+            const itemElement = document.createElement('li')
             // Set the text content of the list item element to the item name
-
+            itemElement.textContent = item.name
             // Attach a click event listener to the list item to add it to the order
-
+            itemElement.addEventListener('click', () => addToOrder(item.name, item.price))
             // Append the list item to the list of items
-
+            listItems.appendChild(itemElement)
+        })
+    }
             
 }
+
 
 // Callback function for adding an item to the order
 function addToOrder(itemName) {
